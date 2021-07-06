9af4f577f5e00a8d710358305dee27d7b7612892
MounishP <mnsh.pv1@gmail.com>
2021-07-06 21:24:51 +0530

    oops Polymorphism

--- inheritance/Single.py
+++ inheritance/Single.py
@@ -1,18 +1,14 @@
 class Parent:
-    def __init__(self):
-        print("This is Parent")
 
-    def property(self):
+    def things(self):
         print("Belong to parent")
 
 
 class Child(Parent):
-    def __init__(self):
-        Parent.__init__(self)
-        print("This is child")
 
     def things(self):
         print("Belong to child")
 
 
-child = Child()
+child = Parent()
+child.things()
--- inheritance/heir.py
+++ inheritance/heir.py
@@ -1,24 +1,47 @@
-class Father:
-    def house(self):
-        print("This is Father")
-
-
-class Prakash(Father):
-    def boy(self):
-        print("This is Prakash")
-
-
-class Renuka(Father):
-    def girl(self):
-        super().house()
-        print("This is Renuka")
-
-
-boy = Prakash()
-boy.boy()
-boy.house()
-
-girl = Renuka()
-girl.girl()
-girl.house()
-
+# class Father:
+#     def house(self):
+#         print("This is Father")
+#
+#
+# class Prakash(Father):
+#     def boy(self):
+#         print("This is Prakash")
+#
+#
+# class Renuka(Father):
+#     def girl(self):
+#         super().house()
+#         print("This is Renuka")
+#
+#
+# boy = Prakash()
+# boy.boy()
+# boy.house()
+#
+# girl = Renuka()
+# girl.girl()
+# girl.house()
+#
+#
+# class A:
+#     def __init__(self, a):
+#         self.a = a
+#
+#     def __add__(self, other):
+#         s = self.a * other.a
+#         return s
+#
+#
+# a = A(1)
+# b = A(3)
+# print(a + b)
+#
+# def method(a, b):
+#     return a + b
+#
+# 
+# def method(a, b, c):
+#     return a + b + c
+#
+#
+# print(method(1, 2, 3))
