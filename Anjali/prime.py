a37a13629f5fae5129cd3399272cee877307d0f1
MounishP <mnsh.pv1@gmail.com>
2021-12-01 20:57:01 +0530

    dec1

--- Anjali/prime.py
+++ Anjali/prime.py
@@ -6,6 +6,8 @@ count = 0
 for n in range(1, 100):
     for i in range(2, n):
         if n % i == 0:
-            count += 1
-    if count == 0:
+            count+=1
+    else:
         print(n)
+
+
