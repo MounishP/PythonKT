2416e4fec1aa00b82538618f6e78862cc4fd41c0
MounishP <mnsh.pv1@gmail.com>
2021-12-14 20:38:00 +0530

    dec14

---
+++ Anjali/listEven.py
@@ -0,0 +1,13 @@
+l = [1, 2, 3, 5, 6, 88, 90, 556, 34523, 2, 5, 9]
+# for i in l:
+#     if i % 2 == 0:
+#         print(i)
+
+for i in range(len(l)):
+    if l[i] % 2 == 0:
+        print(l[i])
+
+
+"""
+1. WAPP to print all the prime numbers from 1 to 1000
+"""
