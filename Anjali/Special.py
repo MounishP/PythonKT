4d48cb5357fc0340f27de70605ce085b055541f6
MounishP <mnsh.pv1@gmail.com>
2021-12-22 20:49:44 +0530

    dec22

---
+++ Anjali/Special.py
@@ -0,0 +1,14 @@
+from math import sqrt
+# import Comparison
+# import math
+"""
+Special methods and special variables are __x__ because these are inbuilt and cant
+be overridden
+"""
+
+a = 45
+a.__add__()
+a.__floor__()
+ # if __name__ = __main__()
+
+sqrt()
---
+++ Anjali/decorators.py
@@ -0,0 +1,13 @@
+"""
+Python Decorators-> which allows us to modify or make changes to the behaviour
+ of the function or class
+"""
+
+
+
+def func_dec():
+    print("I am a function")
+
+
+variable = func_dec
+print(variable())
