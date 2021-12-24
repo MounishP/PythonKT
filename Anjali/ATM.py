632b048eebc3c0ee02168257478af4d9656c6f78
MounishP <mnsh.pv1@gmail.com>
2021-12-24 21:04:08 +0530

    dec24

--- Anjali/ATM.py
+++ Anjali/ATM.py
@@ -1,6 +1,8 @@
 count = 0
-originalPin = 1234
-balance = 10000
+users = ["Mounish", "Anjali", "Aryan"]
+pins = [2223, 4422, 5500]
+balances = [20000, 15000, 25000]
+balance = 0
 
 
 def showBalance():
@@ -26,7 +28,7 @@ def deposit():
 def withdraw():
     global balance
     amount = int(input("Enter the amount to withdraw: "))
-    balance = balance -amount
+    balance = balance - amount
     proceed = int(input("Enter your option(1-continue,2-exit): "))
     if proceed == 1:
         menu()
@@ -35,7 +37,11 @@ def withdraw():
 
 
 def close():
-    print("in close")
+    print("****************************************************")
+    print("                                                    ")
+    print("---Thank you for using Mounish National bank ATM.---")
+    print("                                                    ")
+    print("****************************************************")
 
 
 def menu():
@@ -50,11 +56,41 @@ def menu():
         close()
 
 
-while count != 3:
-    pin = int(input("Enter the pin: "))
-    if pin == originalPin:
-        menu()
-        break
-    else:
-        print("Incorrect Pin")
-        count += 1
+try:
+    while count != 3:
+        pin = int(input("Enter the pin: "))
+        print("")
+
+
+        def digit(pin):
+            temp = pin
+            count = 0
+            while temp != 0:
+                count += 1
+                temp = temp // 10
+
+            if count == 4:
+                return pin
+            else:
+                print("Pin should be 4 digits only")
+
+
+        if digit(pin) in pins:
+            # sl = "select sno from users where pin = 'pin'"
+            if pin == pins[0]:
+                print("Hi ", users[0])
+                balance = balances[0]
+            elif pin == pins[1]:
+                print("Hi ", users[1])
+                balance = balances[1]
+            else:
+                print("Hi ", users[2])
+                balance = balances[2]
+            menu()
+            break
+        else:
+            print("Incorrect Pin")
+            count += 1
+
+except ValueError:
+    print("Enter only numbers...")
---
+++ Anjali/Try.py
@@ -0,0 +1,8 @@
+"""
+Try and except
+"""
+try:
+    a = int(input("Enter the value: "))
+    print(a)
+except Exception:
+    print("enter only numbers not any alphabets")
